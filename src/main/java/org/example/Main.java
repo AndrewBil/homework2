411412dc95b83bf04ba892e3b65fb7d5cd96cf01
andrew <andrew.bilogan@gmail.com>
2023-03-03 11:20:35 +0200

    Added lombok

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.info("Hello {}", "world!");
+        log.debug("Hello {}", "world!");
+        log.trace("Hello {}", "world!");
+        log.error("Hello {}", "world!");
+        log.warn("Hello {}", "world!");
     }
 }
